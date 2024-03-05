85cfa327abe7e80abd2691904e6c7122750bad0f
Tyler Studanski <tyler.studanski@mspmac.org>
2024-03-05 14:48:38 -0600

    Added examples and better definitions

--- common/Elmtify.js
+++ common/Elmtify.js
@@ -1,5 +1,8 @@
 /// Easy function to generate deep elements from text
 /// Copied from https://stackoverflow.com/a/14565049/3416155
+
+/// Example: Creates a <p> element
+///var pElement = elmtify('<p>Test</p>');
 var elmtify = function(html) {
     var wrapper = document.createElement('div');
     wrapper.innerHTML = html;
--- common/WaitFor.js
+++ common/WaitFor.js
@@ -1,6 +1,16 @@
 // Function to simplify the waiting process to run a function
 // If testFunc returns true then runs callback, otherwise will test again after waitTime (in ms) has past
-// If waitTime isn't assigned then 100ms is assumed
+
+/// Example: Checks every 400 ms for the current time of second to be at 10 and then logs the current time to the console
+// waitFor(function() {
+//     return Date().indexOf(':10') != -1;
+// }, function() {
+//     console.log('It is now: ', Date());
+// }, 400);
+
+/// testFunc: Function that returns boolean
+/// callback: Function that's called if testFunc returns TRUE
+/// waitTime: How often to check in milliseconds.  Defaults to 100 ms.
 function waitFor(testFunc, callback, waitTime) {
     if (waitTime == undefined) {
         waitTime = 100;

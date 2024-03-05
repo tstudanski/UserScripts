6712ca6c84d82c98f5ae960eb3d944343d00dd91
Tyler Studanski <tyler.studanski@mspmac.org>
2024-03-05 14:49:54 -0600

    Added `OnChange` method assignment to constructor - Added example

--- common/ChangeModule.js
+++ common/ChangeModule.js
@@ -1,9 +1,23 @@
 /// Plugin to simplify triggering an action when some value changes
 /// Inspired by https://stackoverflow.com/a/1930942/3416155
+
+/// Example: Creates a monitor for the document that logs a textContent change
+// var monitor = new OnChangeModule(500, function(){
+//     return document.textContent;
+// }, function() {
+//     console.log('Value changed from ' + oldValue + ' to ' + newValue);
+// });
+
+/// frequency: How often to check for changes in milliseconds
+/// getValueFunction: function that returns a value
+/// onChange: function that is called if getValueFunction returns a new value.  This function is passed 2 parameters (oldValue, newValue)
 class OnChangeModule {
-    constructor(frequency, getValueFunction) {
+    constructor(frequency, getValueFunction, onChange) {
         this.getValueFunction = getValueFunction;
         this.oldValue = getValueFunction();
+        if (onChange) {
+            this.onChange = onChange;
+        }
         if (frequency == null) {
             frequency = 1000;
         }

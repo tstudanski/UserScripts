31d7adf86023503bde5494ff4271ded4ddc5f5e4
Tyler Studanski <tyler.studanski@mspmac.org>
2024-03-07 15:31:50 -0600

    Fixed userscript matching - Fixed menu button styling - Fixed speed clicking issue

--- Internal-Tools/PwSafe.user.js
+++ Internal-Tools/PwSafe.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Password Safe Beyond Insight
 // @namespace    https://github.com/tstudanski/
-// @version      2024.3.7.0
+// @version      2024.3.7.1
 // @description  Shortcuts for using this password management tool
 // @author       Tyler Studanski
-// @match        https://pwsafe.mac.msp.airport/webconsole/#!/*
+// @match        https://pwsafe.mac.msp.airport/webconsole/*
 // @icon         https://pwsafe.mac.msp.airport/webconsole/favicon.ico?fav=bt
 // @downloadURL  https://github.com/tstudanski/UserScripts/raw/main/Internal-Tools/PwSafe.user.js
 // @updateURL    https://github.com/tstudanski/UserScripts/raw/main/Internal-Tools/PwSafe.user.js
@@ -16,6 +16,7 @@
 
 'use strict';
 class PwSafeModel extends BaseModel {
+    lightningSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightning-fill" viewBox="0 0 16 16" style="color: yellow;width: 25px;height:auto"><path d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641z"></path></svg>';
     constructor() {
         super();
         this.debug('Generated Model');
@@ -39,14 +40,24 @@ class PwSafeModel extends BaseModel {
         if (document.querySelector('#qButtonMax')) {
             document.querySelector('#qButtonMax').remove();
         }
-        var quickButton = elmtify('<div id="qButtonMin"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightning-fill" viewBox="0 0 16 16" style="color: yellow;width: 25px;height:auto"><path d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641z"></path></svg></div>');
+        var quickButton = elmtify('<div id="qButtonMin">' + this.lightningSvg + '</div>');
         
         var self = this;
         quickButton.onclick = function() {
             self.jumpToPassword();
         }
         miniMenu.appendChild(quickButton);
-        quickButton = elmtify('<li id="qButtonMax" _ngcontent-dva-c76 template-generator _nghost-dva-c75 class="odd interactive theme--default ae-cr" data-index="6" aria-setsize="6" aria-posinset="6" tabindex="-1" data-virtualtabstop="false" role="option" aria-label="Quick Password"><wc-lt-appmenucategory class="list-template-component theme--default ae-cr" _nghost-dva-c139><div _ngcontent-dva-c139 class="flex-x h-start v-center wrapper"><ae-icon _ngcontent-dva-c139 role="img" _nghost-dva-c62 aria-hidden="true" class="theme--default ae-cr"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightning-fill" viewBox="0 0 16 16" style="color: yellow;width: 25px;height:auto"><path d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641z"></path></svg></ae-icon><ae-translate _ngcontent-dva-c139="" class="label">Quick Password</ae-translate></div></wc-lt-appmenucategory></li>');
+
+        quickButton = maxMenu.children[0].cloneNode(true);
+        quickButton.id = "qButtonMax";
+        quickButton.removeAttribute('data-assigned-id');
+        quickButton.setAttribute('aria-label', 'Quick Password');
+        quickButton.children[0].removeAttribute('data-ref');
+        var aeIcon = quickButton.querySelector('ae-icon');
+        aeIcon.removeAttribute('data-icon-name');
+        aeIcon.children[0].remove();
+        aeIcon.appendChild(elmtify(this.lightningSvg));
+        quickButton.querySelector('ae-translate').textContent = 'Quick Password';
         
         quickButton.onclick = function() {
             self.jumpToPassword();
@@ -59,16 +70,23 @@ class PwSafeModel extends BaseModel {
             // Go to passwords
             document.querySelector('ae-icon[data-icon-name="menu_passwordsafe"]').click();
         }
+
         // Go into iframe
-        var frame = document.querySelector('iframe#passwordsafeIframe').contentWindow.document;
-        // Select 1st lightning bolt // TODO Change this to make sure it matches the logged in user account
-        frame.querySelectorAll('.icon-action.onecl')[0].click();
-        // Retrieve Password
         waitFor(function() {
-            return frame.querySelector('.btn-retrieve-password-ql');
+            return document.querySelector('iframe#passwordsafeIframe');
         }, function() {
-            frame.querySelector('.btn-retrieve-password-ql').click();
+            var frame = document.querySelector('iframe#passwordsafeIframe').contentWindow.document;
+            // Select 1st lightning bolt // TODO Change this to make sure it matches the logged in user account
+            frame.querySelectorAll('.icon-action.onecl')[0].click();
+            
+            // Retrieve Password
+            waitFor(function() {
+                return frame.querySelector('.btn-retrieve-password-ql');
+            }, function() {
+                frame.querySelector('.btn-retrieve-password-ql').click();
+            })
         })
+        
     }
     onPage(expectedEnding) {
         return document.location.href.indexOf(expectedEnding) != -1;

d80d29b752809c4b43c44bac95602f0540b5d2cb
Tyler Studanski <tyler.studanski@mspmac.org>
2024-04-09 12:13:42 -0500

    Made it easier to change environment

--- common/BaseModel.js
+++ common/BaseModel.js
@@ -1,7 +1,11 @@
 /// Model class that hold commonly used methods
 class BaseModel {
     env = 'prod'
-    constructor() {}
+    constructor(env) {
+        if (env) {
+            this.env = env;
+        }
+    }
     debug(...data) {
         if (this.env != 'prod') {
             console.log(data);

aed5f53c58a1b794a685fba6163da107e58b5b3d
Tyler Studanski <tyler.studanski@mspmac.org>
2024-06-21 15:22:02 -0500

    Updated waitFor to have a limit

--- common/WaitFor.js
+++ common/WaitFor.js
@@ -1,5 +1,6 @@
 // Function to simplify the waiting process to run a function
 // If testFunc returns true then runs callback, otherwise will test again after waitTime (in ms) has past
+// 50 attempts are made before giving up
 
 /// Example: Checks every 400 ms for the current time of second to be at 10 and then logs the current time to the console
 // waitFor(function() {
@@ -11,7 +12,22 @@
 /// testFunc: Function that returns boolean
 /// callback: Function that's called if testFunc returns TRUE
 /// waitTime: How often to check in milliseconds.  Defaults to 100 ms.
-function waitFor(testFunc, callback, waitTime) {
+function waitFor(testFunc, callback, waitTime, id) {
+    var key = id;
+    if (!document.waitForCount) {
+        document.waitForCount = {};
+    }
+    if (!key) {
+        key = Date.now();
+    }
+    if (document.waitForCount[key] == null) {
+        document.waitForCount[key] = 50;
+    } else if (document.waitForCount[key] == 0) {
+        var data = { testFunc: testFunc, callback: callback };
+        console.error('Took too long to execute.', data);
+        return false;
+    }
+    document.waitForCount[key]--;
     if (waitTime == undefined) {
         waitTime = 100;
     }
@@ -20,7 +36,7 @@ function waitFor(testFunc, callback, waitTime) {
     } else {
         console.log('Had to wait');
         setTimeout(function() {
-            waitFor(testFunc, callback, waitTime);
+            waitFor(testFunc, callback, waitTime, key);
         }, waitTime);
     }
 }
